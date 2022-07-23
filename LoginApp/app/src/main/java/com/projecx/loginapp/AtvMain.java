7f4b92461622d473de80e2814ffa9599f7a10322
hs.go <hs.go@gogotech.biz>
2022-07-23 12:51:38 +0900

    hs.go
    [And]
    - 프로젝트 시작.

---
+++ LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
@@ -0,0 +1,14 @@
+package com.projecx.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtvMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atv_main);
+    }
+}
---
+++ LoginApp/app/src/main/java/com/projecx/loginapp/AtvSingUp.java
@@ -0,0 +1,14 @@
+package com.projecx.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtvSingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atv_singup);
+    }
+}
