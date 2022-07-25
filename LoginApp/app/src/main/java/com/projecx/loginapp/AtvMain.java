62b08bb6b1dd0d475ac3638691d13e638d4e4605
hs.go <askhs0302.dev@gmail.com>
2022-07-26 01:07:24 +0900

    [App] - 데이터베이스 생성 및 예제 코드 추가.

--- LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
+++ LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
@@ -1,14 +1,64 @@
+//------------------------------------------------------------------------------------------------//
+//
+//------------------------------------------------------------------------------------------------//
 package com.projecx.loginapp;
 
+//------------------------------------------------------------------------------------------------//
+//
+//------------------------------------------------------------------------------------------------//
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AtvMain extends AppCompatActivity {
+//------------------------------------------------------------------------------------------------//
+//
+//------------------------------------------------------------------------------------------------//
+public class AtvMain extends AppCompatActivity
+{
+    EditText etId;
+    EditText etPwd;
+    Button btnLogin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.atv_main);
+
+        InitLayout();
+        InitEvent();
+        InitDB();
+    }
+    private void InitDB()
+    {
+        DBHelper helper;
+        SQLiteDatabase db;
+        helper = new DBHelper(AtvMain.this, "newdb.db", null, 1);
+        db = helper.getWritableDatabase();
+        helper.onCreate(db);
+    }
+
+    private void InitLayout()
+    {
+        etId = (EditText) findViewById(R.id.editID);
+        btnLogin = (Button) findViewById(R.id.loginbutton);
+    }
+
+    private void InitEvent()
+    {
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                etId.getText()
+            }
+        });
     }
 }
